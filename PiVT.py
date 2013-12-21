c7c089e729bc6f3ba306919a53826bee4b8bb3ac
Sam Nicholson <sam@samn.co.uk>
2013-12-21 00:25:00 +0000

    Proof of concept player tweak to have three videos and three OMXPlayer instances in parallel

--- PiVT.py
+++ PiVT.py
@@ -1,9 +1,8 @@
-from OMXControl import OMXControl
+from OMXControl import OMXControl 
 from time import sleep
-
 if __name__ == '__main__':
     # Launch a simple player
-    test1 = OMXControl(['/videos/Monday_Evening_Live_Titles_HD.mp4', '-s', '--no-osd'])
+    test1 = OMXControl(['/videos/Monday_Evening_Live_Titles_HD.mp4', '-s', '--no-osd', '--win', "0 0 1920 1080"])
     
     while (test1.get_ready() != True):
         pass
@@ -12,26 +11,39 @@ if __name__ == '__main__':
     
     sleep(2)
     
-    test2 = OMXControl(['/videos/James_Bungee_Jump_Export.mp4', '-s', '--no-osd'])
-    
-    while (test1.get_position() < 12):
+    test2 = OMXControl(['/videos/James_Bungee_Jump_Export.mp4', '-s', '--no-osd', '--win', "0 0 1920 1080"])
+    while (test2.get_ready() != True):
+        pass
+    test3 = OMXControl(['/videos/shortclock.mp4', '-s', '--no-osd', '--win', "0 0 1920 1080"])
+
+    while (test1.get_position() < 11.9):
         print test1.get_position()
-    
+
     test1.pause()
     test2.play()
+    print test1.get_position()
+
     test1.stop()
+    
+    sleep(3)
+    del test1
+    sleep(1)
+    test4 = OMXControl(['/videos/FreshersLookback.mp4', '-s', '--no-osd', '--win', "0 0 1920 1080"])
+    while (test4.get_ready() != True):
+        pass
+    test2.pause()
+    test4.play()
+    test2.stop()
+
+    while (test4.get_position() < 10):
+        pass
+    test4.pause()
+    test3.play()
     
     sleep(10)
-    del test1
-    test1 = OMXControl(['/videos/FreshersLookback.mp4', '-s', '--no-osd'])
-    while (test1.get_ready() != True):
-        pass
-    test1.play()
-    test2.pause()
-    test2.stop()
-    
-    sleep(50)
-    test1.stop()
-    del test1
+    test4.stop()
+    test3.stop()
+    del test4
     del test2
+    del test3
     
